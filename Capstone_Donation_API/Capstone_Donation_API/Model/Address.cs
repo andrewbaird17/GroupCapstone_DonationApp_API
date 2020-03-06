b70fba83cbc2ab5adefde1336665d78188e6e486
Zach Mommaerts <zmommaerts@gmail.com>
2020-03-06 08:46:36 -0600

    added properties for models take 2

--- Capstone_Donation_API/Capstone_Donation_API/Model/Address.cs
+++ Capstone_Donation_API/Capstone_Donation_API/Model/Address.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,15 @@ namespace Capstone_Donation_API.Model
 {
     public class Address
     {
+        [Key]
+        public int AddressID { get; set; }
+
+        [ForeignKey("Donor")]
+        public int DonorId { get; set; }
+        public Donor Donor { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
     }
 }
--- Capstone_Donation_API/Capstone_Donation_API/Model/MedicalHistory.cs
+++ Capstone_Donation_API/Capstone_Donation_API/Model/MedicalHistory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,21 @@ namespace Capstone_Donation_API.Model
 {
     public class MedicalHistory
     {
+        [Key]
+        public int MedicalId {get; set;}
+
+        [ForeignKey("Donor")]
+        public int DonorId { get; set; }
+        public Donor Donor { get; set; }
+
+        public string BloodType { get; set; }
+        public bool OnMedications { get; set; }
+        public bool HasAllergies { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public bool IsMale { get; set; }
+        public string Ethnicity { get; set; }
+
+
     }
 }
