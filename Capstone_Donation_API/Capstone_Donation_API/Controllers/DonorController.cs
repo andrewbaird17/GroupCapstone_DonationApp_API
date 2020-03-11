1b4bc92a2e78c8df2d3324f7b7e23307b68d4f53
Andrew Baird <baird.andrew4@gmail.com>
2020-03-11 08:20:44 -0500

    Donor information is updated when passed from MVC app. Refactoring needed

--- Capstone_Donation_API/Capstone_Donation_API/Controllers/DonorController.cs
+++ Capstone_Donation_API/Capstone_Donation_API/Controllers/DonorController.cs
@@ -50,10 +50,47 @@ namespace Capstone_Donation_API.Controllers
             var donorinDB = _context.Donors.Where(d => d.IdentityUserId == donor.IdentityUserId).FirstOrDefault();
             if (ModelState.IsValid)
             {
-                donorinDB = donor;
-                _context.Donors.Update(donor);
+                donorinDB.FirstName = donor.FirstName;
+                donorinDB.LastName = donor.LastName;
+                if (donorinDB.AddressId == null)
+                {
+                    Address newAddress = new Address();
+                    newAddress = donor.Address;
+                    _context.Addresses.Add(newAddress);
+                    _context.SaveChanges();
+                    var addressInDB = _context.Addresses.Where(a => a.StreetAddress == donor.Address.StreetAddress).Where(a => a.City == donor.Address.City)
+                        .Where(a => a.State == donor.Address.State).Where(a => a.ZipCode == donor.Address.ZipCode).FirstOrDefault();
+                    donorinDB.AddressId = addressInDB.Id;
+                }
+                else
+                {
+                    var addressInDB = _context.Addresses.Where(a => a.StreetAddress == donor.Address.StreetAddress).Where(a => a.City == donor.Address.City)
+                        .Where(a => a.State == donor.Address.State).Where(a => a.ZipCode == donor.Address.ZipCode).FirstOrDefault();
+                    donorinDB.AddressId = addressInDB.Id;
+                }
+                if (donorinDB.MedicalId == null)
+                {
+                    MedicalHistory newMedical = new MedicalHistory();
+                    newMedical = donor.MedicalHistory;
+                    _context.MedicalHistories.Add(newMedical);
+                    _context.SaveChanges();
+                    var medicalInDB = _context.MedicalHistories.Where(m => m.Age == donor.MedicalHistory.Age).Where(m => m.BloodType == donor.MedicalHistory.BloodType)
+                        .Where(m => m.Ethnicity == donor.MedicalHistory.Ethnicity).Where(m => m.HasAllergies == donor.MedicalHistory.HasAllergies)
+                        .Where(m => m.Height == donor.MedicalHistory.Height).Where(m => m.IsMale == donor.MedicalHistory.IsMale)
+                        .Where(m => m.OnMedications == donor.MedicalHistory.OnMedications).Where(m => m.Weight == donor.MedicalHistory.Weight).FirstOrDefault();
+                    donorinDB.MedicalId = medicalInDB.Id;
+                }
+                else
+                {
+                    var medicalInDB = _context.MedicalHistories.Where(m => m.Age == donor.MedicalHistory.Age).Where(m => m.BloodType == donor.MedicalHistory.BloodType)
+                        .Where(m => m.Ethnicity == donor.MedicalHistory.Ethnicity).Where(m => m.HasAllergies == donor.MedicalHistory.HasAllergies)
+                        .Where(m => m.Height == donor.MedicalHistory.Height).Where(m => m.IsMale == donor.MedicalHistory.IsMale)
+                        .Where(m => m.OnMedications == donor.MedicalHistory.OnMedications).Where(m => m.Weight == donor.MedicalHistory.Weight).FirstOrDefault();
+                    donorinDB.MedicalId = medicalInDB.Id;
+                }
+                _context.Donors.Update(donorinDB);
                 _context.SaveChanges();
-                return Ok(donor);
+                return Ok(donorinDB);
             }
             return BadRequest();
         }
